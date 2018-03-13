c4996752c2b3bb0e6b2bdb9ee78456519c8a7321
Zack Frank <frank.zack@gmail.com>
2018-03-12 21:05:31 -0500

    Add new model methods friendly_updated_at, full_name, japanese_phone_numbers, and overwrite as_json method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,28 @@
 class Contact < ApplicationRecord
+
+  def friendly_updated_at
+    updated_at.strftime("%A, %B %e %l:%m%p")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def japanese_phone_number
+    "+81 #{phone_number}"
+  end
+
+  def as_json
+    {
+      id: id,
+      first_name: first_name,
+      last_name: last_name,
+      full_name: full_name,
+      email: email,
+      phone_number: japanese_phone_number,
+      created_at: created_at,
+      updated_at: friendly_updated_at
+    }
+  end
+
 end
